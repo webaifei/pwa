aa766cbe3d275369f2f673002d1858345213bfaa
zhangaifei <zhangaifei@51.com>
2018-08-17 18:05:03 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# PWA 10分钟入门
+
+### what is PWA?
+
+### why should you use now?
+
+### how to use?
+
+### tips
+
---
+++ sw.js
@@ -0,0 +1,16 @@
+var cacheName = 'shell-content';
+var filesToCache = [
+  "/assets/styles.css",
+  "/assets/images/078513e9-5751-a466-0937-7a6dc7589149.png",
+  "/assets/js/main.js"
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
