36eda8f7d02782f49c8aa848e9cc0bfbb5c479d3
ngnice <1334806067@qq.com>
2018-09-17 18:13:46 +0800

    Track 1 files into repository.
    - modified examples/demo2/sw.js
    
    Auto commit by GitBook Editor

--- examples/demo2/sw.js
+++ examples/demo2/sw.js
@@ -6,8 +6,11 @@ self.addEventListener('install', event => {
         caches.open('static-v1').then(cache => cache.add('./img/logo-48x48.png'))
     );
 });
-
+// 默认第一次激活sw之后 不会掌控客户端 即不会拦截客户端的请求 出发fetch事件
 self.addEventListener('activate', event => {
+    // 通过调用下面的方法 可以在sw激活之后 立即掌控客户端
+    // 1. 渐进增强 及时没有sw 也应该正常工作
+    // 2. 首次激活的时候 资源未必在cache中
     clients.claim();
     console.log('V1 now ready to handle fetches!');
 });
