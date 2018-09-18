b7b965d57cf9ed6078ec741d00e0e2cc1f591289
ngnice <1334806067@qq.com>
2018-09-18 13:44:35 +0800

    Track 1 files into repository.
    - modified examples/demo3/sw.js
    
    Auto commit by GitBook Editor

--- examples/demo3/sw.js
+++ examples/demo3/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
     console.log('V1 installing…');
-    
+
     // cache a cat SVG
     event.waitUntil(
         caches.open('static-v1').then(cache => cache.addAll(['./img/logo-48x48.png', './img/logo-192x192.png']))
@@ -22,6 +22,6 @@ self.addEventListener('fetch', event => {
     // same-origin and the path is '/dog.svg'
     console.log(url, 'url');
     if (url.origin == location.origin && url.pathname == '/img/logo-192x192.png') {
-        event.respondWith(caches.match('/img/logo-192x192.png'));
+        event.respondWith(caches.match('/img/logo-96x96.png'));
     }
 });
