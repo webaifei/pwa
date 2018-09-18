ab7dd51b4b7d7d689861f7fa2237fd83f3e24553
ngnice <1334806067@qq.com>
2018-09-18 13:55:46 +0800

    Track 1 files into repository.
    - modified examples/demo3/sw.js
    
    Auto commit by GitBook Editor

--- examples/demo3/sw.js
+++ examples/demo3/sw.js
@@ -22,6 +22,6 @@ self.addEventListener('fetch', event => {
     // same-origin and the path is '/dog.svg'
     console.log(url, 'url');
     if (url.origin == location.origin && url.pathname == '/img/logo-192x192.png') {
-        event.respondWith(caches.match('/img/logo-96x96.png'));
+        event.respondWith(caches.match('/img/logo-192x192.png'));
     }
 });
