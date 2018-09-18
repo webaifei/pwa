2333c7b1e5694444ea9ebc0ba988baae35020688
zhangaifei <zhangaifei@51.com>
2018-09-18 15:30:35 +0800

    Updates

--- examples/demo3/sw.1.js
+++ examples/demo3/sw.1.js
@@ -3,14 +3,15 @@ self.addEventListener('install', event => {
 
     // cache a cat SVG
     event.waitUntil(
-        caches.open('static-v1').then(cache => 
-            cache.addAll(['./img/logo-48x48.png', "./img/logo-96x96.png"])
-            
-        )
+        caches.open('static-v1').then(cache => cache.addAll(['./img/logo-48x48.png', './img/logo-192x192.png']))
     );
 });
-
+// 默认第一次激活sw之后 不会掌控客户端 即不会拦截客户端的请求 出发fetch事件
 self.addEventListener('activate', event => {
+    // 通过调用下面的方法 可以在sw激活之后 立即掌控客户端
+    // 1. 渐进增强 及时没有sw 也应该正常工作
+    // 2. 首次激活的时候 资源未必在cache中
+    // clients.claim();
     console.log('V1 now ready to handle fetches!');
 });
 
@@ -21,6 +22,6 @@ self.addEventListener('fetch', event => {
     // same-origin and the path is '/dog.svg'
     console.log(url, 'url');
     if (url.origin == location.origin && url.pathname == '/img/logo-192x192.png') {
-        event.respondWith(caches.match('/img/logo-96x96.png'));
+        event.respondWith(caches.match('/img/logo-192x192.png'));
     }
 });
